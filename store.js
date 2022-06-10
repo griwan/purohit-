5597f0263678a4acc2aa421830dc98e0add13aff
bigyank <iamcience@gmail.com>
2022-06-11 00:39:44 +0545

    changes

--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-import { atom } from "jotai";
+import { atom } from 'jotai';
 
 export const navAtom = atom({
   module: 1,
--- windi.config.js
+++ windi.config.js
@@ -1,28 +1,27 @@
-import { defineConfig } from "windicss/helpers";
+import { defineConfig } from 'windicss/helpers';
 
-import colors from "windicss/colors";
-import plugin from "windicss/plugin";
+import colors from 'windicss/colors';
+import plugin from 'windicss/plugin';
 
 export default defineConfig({
   extract: {
-    include: ["**/*.{js,jsx,tsx,css,mdx}"],
-    exclude: ["node_modules", ".git", ".next"],
+    include: ['**/*.{js,jsx,tsx,css,mdx}'],
+    exclude: ['node_modules', '.git', '.next'],
   },
   attributify: true,
   shortcuts: {
-    btn: "h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800",
+    btn: 'h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800',
   },
   theme: {
     extend: {
       colors: {
         blue: colors.sky,
-        red: "#A9253F",
-        pink: "#eb5e7f",
-        "light-bg": "#F0F0F0",
+        red: '#A9253F',
+        'light-bg': '#F0F0F0',
       },
       fontFamily: {
-        sans: ["Montserrat", "sans-serif"],
-        serif: ["Libre Baskerville", "serif"],
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Libre Baskerville', 'serif'],
       },
     },
   },
