4e4a9cb8adfeb4622676db09e0c42f20c49f78c3
kshitijsubedi <2subedikshitij@gmail.com>
2022-06-12 14:49:30 +0545

    🫡 Quiz issues fixes

--- components/doublelayout.jsx
+++ components/doublelayout.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import classNames from "classnames";
 const DoubleLayout = (props) => {
   return (
-    <div className="flex justify-center pt-8">
+    <div
+      className={classNames("flex pt-8", {
+        "justify-center": props.center,
+        "justify-start": props.left,
+        "justify-end": props.right,
+      })}
+    >
       <div className="flex gap-4 justify-center items-center">
         <div className="w-full">{props.children[0]}</div>
         <div className="w-full">{props.children[1]}</div>
