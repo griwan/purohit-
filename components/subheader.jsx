9d9c456692becce514f71fa72fe623ec14fd7876
bigyank <iamcience@gmail.com>
2022-06-12 22:21:17 +0545

    📈 chart

--- components/subheader.jsx
+++ components/subheader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubHeader = (props) => {
   return (
-    <div className="flex justify-center text-[#707070] mt-[4.5rem] text-[30px] font-sans">
+    <div className="flex justify-centero text-center leading-9 text-[#707070] mt-[4.5rem] text-[30px] font-sans">
       {props.children}
     </div>
   );
